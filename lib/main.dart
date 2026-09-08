0fe25947b4f69ac4713e663beae4b73a26e4e9db
vamsim17 <vamsimarrapu17@gmail.com>
2026-09-08 16:11:47 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Class Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Class Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // This button intentionally does nothing when pressed.
+          onPressed: () {},
+          child: const Text('Press me'),
+        ),
+      ),
+    );
+  }
+}
